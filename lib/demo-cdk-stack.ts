2f164f4bd635c1c26a592544de4e3d8fff31eaaa
lkchoi <lester.k.choi@gmail.com>
2024-06-17 23:42:46 -0400

    feat: read from github repo

--- lib/demo-cdk-stack.ts
+++ lib/demo-cdk-stack.ts
@@ -1,6 +1,8 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { SecretValue, Stack, StackProps } from 'aws-cdk-lib';
 import { CodeBuildStep, CodePipeline, CodePipelineSource } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+
+import { git } from './git';
 
 export class DemoCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -9,7 +11,9 @@ export class DemoCdkStack extends Stack {
     new CodePipeline(this, 'DemoPipeline', {
       pipelineName: 'DemoPipeline',
       synth: new CodeBuildStep('Synth', {
-        input: CodePipelineSource.gitHub('lkchoi/demo-sam', 'main'),
+        input: CodePipelineSource.gitHub('lkchoi/demo-sam', 'main', {
+          authentication: SecretValue.secretsManager(git.secretArn)
+        }),
         commands: [
           'npm ci', // install dependencies
           'npm run build', // build the api
---
+++ lib/git.ts
@@ -0,0 +1,13 @@
+export const git = {
+  // GitHub info
+  owner: 'lkchoi',
+  cdkRepository: 'clouda-cdk',
+
+  // Secrets stored in AWS Secret Manager
+  secretArn: 'arn:aws:secretsmanager:us-east-1:552934060812:secret:gh-lkchoi-pat-0KblwD',
+  githubTokenKey: 'gh-lkchoi-pat',
+}
+
+/* ARN: arn:aws:secretsmanager:us-east-1:552934060812:secret:gh-lkchoi-pat-0KblwD
+Name: gh-lkchoi-pat
+VersionId: f652e973-d581-4e51-94a9-2af3c8b55e79 */
