6f1cfca7d950d4ce0a61c3d56ac280fbad82b03b
lkchoi <lester.k.choi@gmail.com>
2024-06-19 00:36:03 -0400

    --wip-- [skip ci]

--- lib/git.ts
+++ lib/git.ts
@@ -4,6 +4,6 @@ export const git = {
   cdkRepository: 'clouda-cdk',
 
   // Secrets stored in AWS Secret Manager
-  secretArn: 'arn:aws:secretsmanager:us-east-1:552934060812:secret:01j0qc41bmzpmqz6ett2hmpv33-XtevPi',
+  secretArn: 'arn:aws:secretsmanager:us-east-1:552934060812:secret:01j0qceh5zqzzqnd13kk4a4j7t-3COMtz',
   githubTokenKey: 'token',
 }
