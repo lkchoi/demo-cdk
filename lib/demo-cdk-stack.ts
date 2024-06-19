a4a78bd25f33b2519029901b5595a7d89e055aba
lkchoi <lester.k.choi@gmail.com>
2024-06-19 12:59:23 -0400

    move "Build API" to separate pipeline build step

--- lib/demo-cdk-stack.ts
+++ lib/demo-cdk-stack.ts
@@ -9,26 +9,45 @@ export class DemoCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const cdkRepo = CodePipelineSource.gitHub('lkchoi/demo-cdk', 'main', {
+      authentication: SecretValue.secretsManager(git.secretArn, { jsonField: 'token' })
+    });
+
+    const apiRepo = CodePipelineSource.gitHub('lkchoi/demo-sam', 'main', {
+      authentication: SecretValue.secretsManager(git.secretArn, { jsonField: 'token' })
+    });
+
     const pipeline = new CodePipeline(this, 'DemoPipeline', {
       pipelineName: 'DemoPipeline',
       synth: new CodeBuildStep('Synth', {
-        input: CodePipelineSource.gitHub('lkchoi/demo-cdk', 'main', {
-          authentication: SecretValue.secretsManager(git.secretArn, { jsonField: 'token' })
-        }),
-        additionalInputs: {
-          '../packages/demo-sam': CodePipelineSource.gitHub('lkchoi/demo-sam', 'main', {
-            authentication: SecretValue.secretsManager(git.secretArn, { jsonField: 'token' })
-          })
-        },
+        input: cdkRepo,
+        // additionalInputs: { '../target': apiRepo },
         commands: [
           'npm ci', // install dependencies
           'npm run build', // tsc the cdk
-          'pwd',
-          'ls',
           'npx cdk synth', // synth the cfn template
         ]
       }),
     });
+
+    pipeline.addWave('Build API', {
+      post: [
+        new CodeBuildStep('BuildApiStep', {
+          input: cdkRepo,
+          additionalInputs: { '../target': apiRepo },
+          commands: [
+            'pwd',
+            'ls',
+            'pushd ../target',
+            'pwd',
+            'ls',
+            'npm ci',
+            'npm run build',
+            'ls'
+          ]
+        })
+      ]
+    })
 
     const beta = new DemoService(this, 'DemoService', {});
     pipeline.addStage(beta);
